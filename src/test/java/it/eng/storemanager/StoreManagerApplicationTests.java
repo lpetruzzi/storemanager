015d2300d337acc3fbe0aa794ee8f52eda3f2972
lpetruzzi <luca.petruzzi@eng.it>
2022-07-29 15:57:48 +0200

    initial commit

---
+++ src/test/java/it/eng/storemanager/StoreManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package it.eng.storemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
